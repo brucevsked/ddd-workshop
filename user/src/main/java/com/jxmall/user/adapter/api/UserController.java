70df6a1ba0b274eb3ceafe225c50b37f1b793cd3
Chenbao wu <chen02xw@qq.com>
2019-11-14 15:47:51 +0800

    [user] modify api path

--- user/src/main/java/com/jxmall/user/adapter/api/UserController.java
+++ user/src/main/java/com/jxmall/user/adapter/api/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,26 +19,26 @@ import com.jxmall.user.domain.aggregate.user.UserServices;
 import com.jxmall.user.domain.aggregate.user.root.User;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserServices userServices;
-    
+
     @GetMapping
     public ResponseEntity<List<User>> search(@RequestParam("key") String key) {
         List<User> userList = userServices.search(key);
         return ResponseEntity.ok(userList);
     }
 
-    @PostMapping 
-    public ResponseEntity<String> create(@RequestBody User user, UriComponentsBuilder builder){
+    @PostMapping
+    public ResponseEntity<String> create(@RequestBody User user, UriComponentsBuilder builder) {
         user.setId(UUID.randomUUID().toString());
         return ResponseEntity.created(builder.path("/users/{id}").buildAndExpand(user.getId()).toUri()).build();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> load(@PathParam("id") String id){
+    public ResponseEntity<User> load(@PathParam("id") String id) {
         System.out.println("id : " + id);
         return ResponseEntity.ok(new User());
     }
